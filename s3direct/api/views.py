f600dd5474df67699b29ea852a9d4df88bb8d25d
stanislavkhlud <stasontos2@hotmail.com>
2019-11-06 08:46:22 +0700

    Improve docs for S3DirectWrapper

--- s3direct/api/views.py
+++ s3direct/api/views.py
@@ -4,16 +4,23 @@ from ..api import serializers, utils
 
 
 class S3DirectWrapper(APIView):
-    """Custom s3direct `get_params` method
+    """Custom s3direct `get_params` method.
 
-    It calls own (overridden) s3direct functions.
+    It calls s3direct functions.
 
     This view allows to get request params as JSON instead of form-data and add
-    custom policy expiration time and key equal condition to policy
+    custom policy expiration time and key equal condition to policy.
     """
 
     def post(self, request):
-        """Wrapper method to get upload params for AWS S3 file upload"""
+        """Get parameters for upload to S3 bucket.
+
+        Current endpoint returns all required for direct s3 upload data,
+        which should be later sent to `form_action` as `form-data` url with
+        'file'. Workflow: First, you make request to this endpoint. Then send
+        response data to `form_action` via `POST`.
+
+        """
         serializer = serializers.S3DirectSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
