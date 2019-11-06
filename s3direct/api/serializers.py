5ef46c9a2177f18f09b1744ce93c6e1997fe01b1
stanislavkhlud <stasontos2@hotmail.com>
2019-11-06 08:23:55 +0700

    Add S3UploadSerializer(for S3DirectWrapper auto swagger documentation)

--- s3direct/api/serializers.py
+++ s3direct/api/serializers.py
@@ -15,3 +15,35 @@ class S3DirectSerializer(serializers.Serializer):
     )
     filename = serializers.CharField(required=False, allow_null=True)
     content_type = serializers.CharField(required=False)
+
+
+class S3UploadSerializer(serializers.Serializer):
+    """Serializer for S3DirectWrapper auto swagger documentation.
+
+    This serializer used just for drf-yasg package, so that front-end team
+    could see specs for response for S3DirectWrapper view.
+
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        fields = (
+            'policy',
+            'success_action_status',
+            'x-amz-credential',
+            'x-amz-date',
+            'x-amz-signature',
+            'x-amz-algorithm',
+            'form_action',
+            'key',
+            'acl',
+            'x-amz-security-token',
+            'content-type',
+            'Cache-Control',
+            'Content-Disposition',
+        )
+        for field in fields:
+            self.fields[field] = serializers.CharField(
+                label=field,
+                required=False,
+            )
